62a9a733351361648857cc33f941e17ce39b53b1
wcardero <wcarderor@gmail.com>
2026-02-18 01:35:07 -0500

    Fix para que se ejecute en local desde no-localhost

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,7 @@ runMigrations().then(() => {
 });
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = parseInt(process.env.PORT || '3001', 10);
 
 app.use(cors());
 app.use(express.json());
